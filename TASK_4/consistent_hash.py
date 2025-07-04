226d493bbbd6f528034da8e1a663da78e4d6788c
Fidelisaboke <fidelisaboke@gmail.com>
2025-07-04 10:44:22 +0300

    fix: update consistent hash to use quadratic probing (#3)

--- TASK_4/consistent_hash.py
+++ TASK_4/consistent_hash.py
@@ -5,7 +5,7 @@ class ConsistentHashRing:
         self.slots = slots
         self.virtual_nodes = virtual_nodes
         self.hash_ring = [None] * slots
-        self.server_map = {}  # {slot: server_id}
+        self.server_map = {}
         self._initialize_ring()
 
     def H(self, i):
@@ -20,20 +20,24 @@ class ConsistentHashRing:
     def _initialize_ring(self):
         for server_id, server_address in enumerate(self.servers):
             for replica_id in range(self.virtual_nodes):
-                slot = self.Phi(server_id, replica_id)
-                original_slot = slot
+                original_slot = self.Phi(server_id, replica_id)
+                slot = original_slot
+                i = 1
                 while self.hash_ring[slot] is not None:
-                    slot = (slot + 1) % self.slots  # Linear probing
-                    if slot == original_slot:
+                    slot = (original_slot + i + (i * i)) % self.slots  # Quadratic probing
+                    i += 1
+                    if i >= self.slots:
                         raise Exception("Hash ring is full.")
                 self.hash_ring[slot] = server_address
                 self.server_map[slot] = server_address
 
     def get_server_for_request(self, request_id):
-        slot = self.H(request_id)
-        original_slot = slot
+        original_slot = self.H(request_id)
+        slot = original_slot
+        i = 1
         while self.hash_ring[slot] is None:
-            slot = (slot + 1) % self.slots
-            if slot == original_slot:
+            slot = (original_slot + i + (i * i)) % self.slots  # Quadratic probing
+            i += 1
+            if i >= self.slots:
                 return None
         return self.hash_ring[slot]
